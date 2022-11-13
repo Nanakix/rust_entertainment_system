53404967a7adfd2a0eae7dcbe4def1e6771d2d3f
Nanakix <goldensun2000@hotmail.fr>
2022-11-14 00:57:37 +0100

    delete target

--- res/target/debug/deps/res-405a427f4c30c25c.d
+++
@@ -1,5 +0,0 @@
-/mnt/y/Users/Jean/Documents/VSCode Projects/rust_entertainment_system/res/target/debug/deps/res-405a427f4c30c25c: src/main.rs
-
-/mnt/y/Users/Jean/Documents/VSCode Projects/rust_entertainment_system/res/target/debug/deps/res-405a427f4c30c25c.d: src/main.rs
-
-src/main.rs:
--- res/target/debug/res.d
+++
@@ -1,1 +0,0 @@
-/mnt/y/Users/Jean/Documents/VSCode\ Projects/rust_entertainment_system/res/target/debug/res: /mnt/y/Users/Jean/Documents/VSCode\ Projects/rust_entertainment_system/res/src/main.rs
