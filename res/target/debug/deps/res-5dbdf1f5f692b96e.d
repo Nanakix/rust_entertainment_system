f8ee1d67a9a4f9e4d6f04c079d7b861c568c07c4
Nanakix <goldensun2000@hotmail.fr>
2022-11-14 00:53:37 +0100

    init main

---
+++ res/target/debug/deps/res-5dbdf1f5f692b96e.d
@@ -0,0 +1,5 @@
+/mnt/y/Users/Jean/Documents/VSCode Projects/rust_entertainment_system/res/target/debug/deps/res-5dbdf1f5f692b96e: src/main.rs
+
+/mnt/y/Users/Jean/Documents/VSCode Projects/rust_entertainment_system/res/target/debug/deps/res-5dbdf1f5f692b96e.d: src/main.rs
+
+src/main.rs:
